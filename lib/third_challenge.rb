5201bb0a5f4503ecc8826aaa236e5ed40403a35f
Toni Lamont <tlam212@gmail.com>
2018-06-19 20:22:18 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -6,7 +6,7 @@ def third_challenge
       :hero => {:name => "Romeo", :age => "15", :status => "alive"},
       :hero_friends => [
         {:name => "Benvolio", :age => "17", :attitude => "worried"},
-        {:name => "Mercutio", :age => "18", :attitude => "hot-headed" ]
+        {:name => "Mercutio", :age => "18", :attitude => "hot-headed"} ]
    }, 
    :capulet => {
       :patriarch => {:name => "Lord Capulet", :age => "50" },
